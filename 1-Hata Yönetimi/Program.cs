f548d2c9e2cf3bd1beef4f9f5b960f38edfe53ab
Murat Toğunçhan Düzgün <togunchan@gmail.com>
2021-02-26 18:47:18 +0300

    Add files via upload

---
+++ 1-Hata Yönetimi/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1_Hata_Yönetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // var liste = new List<string>()
+            // {
+            //     "1","2","5a","10b","abc","10","50"
+            // };
+
+            // foreach (var item in liste)
+            // {
+            //     try
+            //     {
+            //         int a = int.Parse(item);
+            //         Console.WriteLine(a);
+            //     }
+            //     catch (Exception)
+            //     {
+            //         continue;
+            //     }
+            // }
+
+            /****************************************************/
+
+            // while (true)
+            // {
+            //     Console.WriteLine("Sayı giriniz: ");
+            //     string input = Console.ReadLine();
+
+            //     if (input == "q")
+            //     {
+            //         break;
+            //     }
+
+            //     try
+            //     {
+            //         int sayi = int.Parse(input);
+            //         Console.WriteLine("Girdiğiniz sayı: {0}",sayi);
+            //     }
+            //     catch (Exception)
+            //     {
+            //         Console.WriteLine("Geçersiz sayı");
+            //     }
+            // }
+
+            /****************************************************/
+
+            Console.Write("Parola giriniz: ");
+            string parola = Console.ReadLine();
+
+            try
+            {
+                CheckPassword(parola);
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+
+        }
+
+        static void CheckPassword(string input)
+        {
+            string turkce_karakterler = "ğ,Ğ,ç,Ç,ş,Ş,ü,Ü,ö,Ö,ı,İ";
+
+            foreach (char karakter in input)
+            {
+                if (turkce_karakterler.IndexOf(karakter) > -1)
+                {
+                    throw new Exception("Parola Türkçe karakter içeremez");
+                }
+            }
+            Console.WriteLine("Geçerli parola");
+        }
+    }
+}
---
+++ 2-Custom Exception/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _2_Custom_Exception
+{
+    class LoginException : Exception
+    {
+        public LoginException(string message) : base(message)
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Login("sadikturan", "123456");
+            }
+            catch (LoginException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void Login(string name, string password)
+        {
+            if (name.Contains(" "))
+                throw new LoginException("username boşluk içemerez!");
+            if (password.Length < 8)
+                throw new LoginException("Parola min. 8 karakter olmalıdır!");
+
+        }
+    }
+}
